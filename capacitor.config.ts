6db61d8c4e5217dec8144fc0b9017882142a9b3d
luchitodmj <lu.montecinosj@duocuc.cl>
2025-06-08 11:39:55 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projectmobile',
+  webDir: 'www'
+};
+
+export default config;
